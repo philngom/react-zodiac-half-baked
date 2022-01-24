d2ad76f16bce826b85c2f60c9ccad94a50843ba1
Philippe Ngom <philngom@gmail.com>
2022-01-24 15:46:40 -0800

    zodiac card rendered

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,6 +2,5 @@ import React from 'react'
 import './Header.css'
 
 export default function Header(props) {
-  console.log(props)
   return <header>Alchemy Astrology, by {props.name}</header>
 }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import backgroundImg from '../background.png'
 import './Main.css'
+import ZodiacCard from '../ZodiacCard/ZodiacCard'
 
-export default function Main() {
+export default function Main(props) {
+  console.log(props)
   return (
     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-      {/* import the ZodiacCard component */}
-      {/* get the zodiac array from props */}
-      {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
-      {/* remember that when you map over an array and render out a list, each item in the list needs a unique 'key' prop */}
+      {
+        props.zodiac.map((sign, i) =>
+          <ZodiacCard
+            key={sign.name + i}
+            {...sign}
+          />
+        )
+      }
     </main>
   )
 }
+// {/* import the ZodiacCard component */}
+//       {/* get the zodiac array from props */}
+//       {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
+//       {/* remember that when you map over an array and render out a list, each item in the list needs a unique 'key' prop */}
